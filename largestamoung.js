ec4d132732f6355978d562891435cac9e3d4acf8
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-23 09:43:43 +0530

    c2

---
+++ largestamoung.js
@@ -0,0 +1,17 @@
+var num1 = 4
+var num2 = 6
+var num3 = 8
+
+if (num1 >num2 && num1 > num3){
+    console.log(num1,'is largest');
+    
+}
+
+else if (num2> num1 && num2>num3){
+    console.log(num2,'largest');
+    
+}
+else{
+    console.log(num3,'is largest');
+    
+}
