7f7c5fd9fe749901602e566bfec0d49e289528c5
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-23 09:41:04 +0530

    first comit

---
+++ sample.js
@@ -0,0 +1,16 @@
+let m = 4
+
+console.log(m)
+
+
+num1 = 4
+
+console.log(num1++)
+console.log(num1)
+
+
+a = 5
+console.log(a === '5');
+// check both value and data type
+
+
---
+++ units.js
@@ -0,0 +1,20 @@
+unit = 174
+
+if (unit <=100){
+    result = unit *1
+    console.log(result);
+    
+}
+
+else if(unit >100 && unit <200){
+    result = unit*2
+    console.log(result);
+    
+}
+
+else{
+
+    result = unit*3
+    console.log(result);
+    
+}
