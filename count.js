69093eecdc9d029df1c49e78809c12554cb659a1
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-24 10:21:10 +0530

    c7

--- count.js
+++ count.js
@@ -1,13 +1,22 @@
 
 
-let count = 0
+// let count = 0
 
-for(i=1;i<=50; i++){
-    if( i%3 == 0){
-        count+=1
-    }
-}
-console.log(count);
+// for(i=1;i<=50; i++){
+//     if( i%3 == 0){
+//         count+=1
+//     }
+// }
+// console.log(count);
 
 
 // count the total number of character in the string
+
+
+let text = "javascript"
+count = 0
+for(let i=0;i<text.length;i++){
+    count+=1
+    
+}
+    console.log(count);
