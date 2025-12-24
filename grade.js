862749c7c9d6db62653a4e4f9aecfdc3d7005965
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-24 09:37:04 +0530

    wg

---
+++ grade.js
@@ -0,0 +1,17 @@
+var total_mark = 70;
+
+if (total_mark >= 90) {
+    console.log('A');
+} 
+else if (total_mark >= 80) {
+    console.log('B');
+} 
+else if (total_mark >= 70) {
+    console.log('C');
+} 
+else if (total_mark >= 60) {
+    console.log('D');
+} 
+else {
+    console.log('Failed');
+}
