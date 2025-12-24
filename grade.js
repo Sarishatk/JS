2693303d454ec0285a517519200fca690e50dfbf
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-24 09:54:41 +0530

    c6

--- grade.js
+++ grade.js
@@ -29,3 +29,16 @@ else {
 
 
 // }
+
+// for(i = 10; i >=1; i--){
+//     console.log(i);
+    
+// }
+
+for (i = 1;i <=20 ;i++){
+    
+    if(i%2 == 0){
+        console.log(i);
+        
+    }
+}
