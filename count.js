2ca83b065ee2b823e3221046d17ddb133cb45ca9
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-24 10:27:56 +0530

    c8

--- count.js
+++ count.js
@@ -13,12 +13,24 @@
 // count the total number of character in the string
 
 
-let text = "javascript"
-count = 0
-for(let i=0;i<text.length;i++){
-    count+=1
+// let text = "javascript"
+// count = 0
+// for(let i=0;i<text.length;i++){
+//     count+=1
     
-}
-    console.log(count);
+// }
+//     console.log(count);
 
     // return count of specific character in a string
+
+let text = "mississippii";
+let target = "s";
+let count = 0;
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i] === target) {
+        count++;
+    }
+}
+
+console.log(count);
