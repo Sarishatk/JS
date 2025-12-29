2e7bb4fcb0ef336be39531bb6bb7947afe77e572
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-29 10:34:40 +0530

    c9

--- primerange.js
+++ primerange.js
@@ -1,27 +1,82 @@
 // check the prime numbers given range
 
-let start = 1
+// let start = 1
 
-let stop = 50
+// let stop = 50
 
-for (let num = start; num <= stop; num++) {
+// for (let num = start; num <= stop; num++) {
 
-    if (num > 1) {
+//     if (num > 1) {
 
-        let is_prime = true;
+//         let is_prime = true;
 
-        for (let i = 2; i < num; i++) {
+//         for (let i = 2; i < num; i++) {
 
-            if (num % i === 0) {
+//             if (num % i === 0) {
 
-                is_prime = false
+//                 is_prime = false
 
-                break;
-            }
+//                 break;
+//             }
+//         }
+
+//         if (is_prime) {
+//             console.log(num);
+//         }
+//     }
+// }
+
+
+// identify the total number of prime numbers in a given range
+
+// let start = 1;
+// let stop= 30;
+// let count = 0
+
+// for(let num = start;num<=stop;num++){
+
+//     if(num>1){
+//         let is_prime = true
+//        for (let i = 2; i < num; i++){
+//         if(num%i == 0){
+//             is_prime = false
+//             break
+//         }
+//        }
+//        if(is_prime){
+//         count++
+//        }
+//     }
+// }
+// console.log("Total prime numbers:", count);
+
+
+
+// identify the largest prime number in given range
+let start = 1;
+let stop= 30;
+let largest_prime = 1
+
+for(let num = start;num<=stop;num++){
+
+    if(num>1){
+
+        let is_prime = true
+       for (let i = 2; i < num; i++){
+        if(num%i == 0){
+            is_prime = false
+            break
         }
-
-        if (is_prime) {
-            console.log(num);
-        }
+       }
+       if(is_prime){
+      largest_prime = num;
+        
+       }
     }
 }
+if (largest_prime !== 1) {
+    console.log("largest:", largest_prime);
+} else {
+    console.log("not found");
+}
+
