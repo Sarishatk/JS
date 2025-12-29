e5ce71c66585e0ca64da1c67f86457f6c955966b
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-29 09:21:24 +0530

    prime number

--- position.js
+++ position.js
@@ -1,3 +1,10 @@
 // print the elements at  the even index postitions
 
 let text = "javascript"
+
+for(let i=0; i<text.length;i++){
+    if(i %2==0){
+        console.log(text[i]);
+        
+    }
+}
