26d3ed072847093e099f926bb6aee9e45a9689fe
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-31 09:39:10 +0530

    // find the largest number in the given array

---
+++ max.js
@@ -0,0 +1,15 @@
+// find the largest number in the given array
+numbers = [1,3,4,5,7,8,9]
+
+largets_number = numbers[0]
+
+for(i = 0;i < numbers.length;i++){
+    if(numbers[i]> largets_number){
+        largets_number = numbers[i]
+    }
+}
+console.log(largets_number);
+
+
+
+// 
