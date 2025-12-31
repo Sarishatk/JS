633ee7490ccd984f8d487fe128692b7e5e1735be
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-31 10:17:18 +0530

    // sum of all elemeths in the array

--- max.js
+++ max.js
@@ -51,25 +51,34 @@
 // create a new array which contains only the uniuq elements from the above array
 
 
-let numbers = [3,4,5,6,8,13,1,5]
+// let numbers = [3,4,5,6,8,13,1,5]
 
-let even_count = 0
+// let even_count = 0
 
-let odd_count = 0
+// let odd_count = 0
 
-for(i=0;i<=numbers.length;i++){
+// for(i=0;i<=numbers.length;i++){
 
-    if(numbers[i]%2 == 0){
-        even_count+=1
-    }
-    else{
-        odd_count+=1
-    }
+//     if(numbers[i]%2 == 0){
+//         even_count+=1
+//     }
+//     else{
+//         odd_count+=1
+//     }
 
 
-}
-console.log('even count',even_count);
-console.log('odd count', odd_count);
+// }
+// console.log('even count',even_count);
+// console.log('odd count', odd_count);
 
 // sum of all elemeths in the array
 
+ numbers = [3,4,5,6,8,13,1,5]
+
+ let sum =0
+
+ for(i=0;i<numbers.length;i++){
+    sum+=numbers[i]
+ }
+ console.log(sum);
+ 
