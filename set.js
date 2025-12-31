6c045636afcb75b718901a82249c4d3e10fb289b
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-31 10:41:29 +0530

    //add the odd numbers to new list

--- set.js
+++ set.js
@@ -1,3 +1,17 @@
-let elements = new Set([1,0,2,2,8,9])
+// let elements = new Set([1,0,2,2,8,9])
 
-console.log(elements);
+// console.log(elements);
+
+
+//add the odd numbers to new list
+
+let number = [5,6,2,4,6,3,2,4]
+
+let odd_num = new Set([])
+
+for(i=0;i<number.length;i++){
+    if(number[i]%2 !=0){
+        odd_num.add(number[i])
+    }
+}
+console.log(odd_num);
