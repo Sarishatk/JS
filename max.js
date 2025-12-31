5e7e6aee54cc685b4ed9d5de6a4f75c4c8d35eab
Sarisha.tk <sarishakrishna58@gmail.com>
2025-12-31 09:42:39 +0530

    // find the smallest elements in the above array

--- max.js
+++ max.js
@@ -1,15 +1,25 @@
 // find the largest number in the given array
-numbers = [1,3,4,5,7,8,9]
+// numbers = [1,3,4,5,7,8,9]
 
-largets_number = numbers[0]
+// largets_number = numbers[0]
+
+// for(i = 0;i < numbers.length;i++){
+//     if(numbers[i]> largets_number){
+//         largets_number = numbers[i]
+//     }
+// }
+// console.log(largets_number);
+
+
+
+// find the smallest elements in the above array
+
+numbers = [1,3,4,5,7,8,9]
+smallest = numbers[0]
 
 for(i = 0;i < numbers.length;i++){
-    if(numbers[i]> largets_number){
+    if(numbers[i]< smallest){
         largets_number = numbers[i]
     }
 }
-console.log(largets_number);
-
-
-
-// 
+console.log(smallest);
