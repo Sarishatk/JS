f913b7cf33a708429f97772df85b75751d9aba01
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-02 10:27:48 +0530

    done

--- apperanceofword.js
+++ apperanceofword.js
@@ -1,1 +1,14 @@
 // how many times each word apperas
+
+
+let text = "javascripts is easy and javascript is powerful"
+let count = new Map()
+
+let words = text.split(" ")
+
+for(let i of words){
+
+count.set(i, (count.get(i) || 0) + 1)
+}
+
+console.log(count);
