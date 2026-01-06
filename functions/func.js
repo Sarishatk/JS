7b8c4393dc6f380dd3e2a9cc17c7add792f98bea
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-06 09:53:30 +0530

    return statment

--- functions/func.js
+++ functions/func.js
@@ -19,3 +19,14 @@ greet()
 
 // func with parameter
 // parameter is a variable which accept the value when the function is called
+
+function add(num1,num2){
+    sum = num1+num2
+    
+    // return sum   (which represent a specific value (outside) where the function is called)
+    console.log('hello');
+
+}
+let result = add(2,3)
+console.log((result));
+
