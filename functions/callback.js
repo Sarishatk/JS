2d0917f8a75881ed4c619ab1ae1c2e57b7df8d7e
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-06 10:55:20 +0530

    // execution one function is depend on another // result is not avilable immediately // used for asynchronous functions

--- functions/callback.js
+++ functions/callback.js
@@ -20,27 +20,46 @@
 // define a function to chcek wether a given number is prime or bot
 
 
-function primeOrNot(number) {
-    if (number <= 1) {
-        
-        console.log("not a prime");
-        return;
-    }
+// function primeOrNot(number) {
+//     if (number <= 1) {
 
-    for (i = 2; i < number; i++) {
+//         console.log("not a prime");
+//         return;
+//     }
 
-        if (number % i === 0) {
+//     for (i = 2; i < number; i++) {
 
-            console.log("not a prime");
+//         if (number % i === 0) {
 
-            return;
+//             console.log("not a prime");
 
-        }
-    }
+//             return;
 
-    console.log("prime");
+//         }
+//     }
+
+//     console.log("prime");
+// }
+
+// primeOrNot(4); 
+// primeOrNot(7); 
+// primeOrNot(9); 
+
+
+// call back function using parameters
+
+function mainFunction(callback,n1,n2){
+    callback(n1,n2)
 }
 
-primeOrNot(4); 
-primeOrNot(7); 
-primeOrNot(9); 
+function add(n1,n2){
+    sum = n1+n2
+    console.log(sum);
+    
+}
+mainFunction(add,10,20)
+
+
+// execution one function is depend on another
+// result is not avilable immediately
+// used for asynchronous functions
