8adfaeec450127443c863c92f0d7fe2f34435fcb
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-06 10:00:23 +0530

    // define a function to check whether the largest among three numbers

--- functions/func.js
+++ functions/func.js
@@ -20,13 +20,34 @@ greet()
 // func with parameter
 // parameter is a variable which accept the value when the function is called
 
-function add(num1,num2){
-    sum = num1+num2
+// function add(num1,num2){
+//     sum = num1+num2
     
-    // return sum   (which represent a specific value (outside) where the function is called)
-    console.log('hello');
+//     // return sum   (which represent a specific value (outside) where the function is called)
+//     console.log('hello');
 
+// }
+// let result = add(2,3)
+// console.log((result));
+
+// define a function to check whether the largest among three numbers
+
+function largest(a,b,c){
+    if(a >b && a>c){
+        console.log(a);
+        
+    }
+    else if(b>a&&b>c){
+        console.log(b);
+        
+    }
+    else{
+        console.log(c);
+        
+    }
+    
 }
-let result = add(2,3)
-console.log((result));
+largest(4,6,7)
 
+
+// 
