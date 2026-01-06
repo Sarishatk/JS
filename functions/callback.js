5763c6e4e2dc04a782229bb2e6d300ef67abd280
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-06 10:36:38 +0530

    done

--- functions/callback.js
+++ functions/callback.js
@@ -4,17 +4,43 @@
 //  A function(b) is passed an argument for a main function (A) in which A will be Executed later
 
 
-function greet(callback){
-    console.log('hello sarisha');
-    callback();   // calling the passed function
-}
+// function greet(callback){
+//     console.log('hello sarisha');
+//     callback();   // calling the passed function
+// }
 
-function sayhello(){
-    console.log('bye');
-}
+// function sayhello(){
+//     console.log('bye');
+// }
 
-greet(sayhello);
+// greet(sayhello);
 
 
 
 // define a function to chcek wether a given number is prime or bot
+
+
+function primeOrNot(number) {
+    if (number <= 1) {
+        
+        console.log("not a prime");
+        return;
+    }
+
+    for (i = 2; i < number; i++) {
+
+        if (number % i === 0) {
+
+            console.log("not a prime");
+
+            return;
+
+        }
+    }
+
+    console.log("prime");
+}
+
+primeOrNot(4); 
+primeOrNot(7); 
+primeOrNot(9); 
