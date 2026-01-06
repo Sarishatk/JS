53606f58c85b630f2ced96cbea24f4f074a34156
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-06 10:30:06 +0530

    // define a function to chcek wether a given number is prime or bot

--- functions/callback.js
+++ functions/callback.js
@@ -1,2 +1,20 @@
 
 // call back function
+
+//  A function(b) is passed an argument for a main function (A) in which A will be Executed later
+
+
+function greet(callback){
+    console.log('hello sarisha');
+    callback();   // calling the passed function
+}
+
+function sayhello(){
+    console.log('bye');
+}
+
+greet(sayhello);
+
+
+
+// define a function to chcek wether a given number is prime or bot
