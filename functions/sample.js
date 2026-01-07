23c54e1ed8b0c2068ca86d760320d4e56be8035f
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-07 09:40:35 +0530

    //  create a function to return the total number of digit in a  number

--- functions/sample.js
+++ functions/sample.js
@@ -1,13 +1,30 @@
 // create a function to return the sum of digitd of a agiven number
 
-function SumOfDigits(num){
-sum = 0
-    while(num>0){
-        digit = num%10
-        sum+=digit
-        num=Math.floor(num / 10);
-    }
-console.log(sum);
+// function SumOfDigits(num){
+// sum = 0
+//     while(num>0){
+//         digit = num%10
+//         sum+=digit
+//         num=Math.floor(num / 10);
+//     }
+// console.log(sum);
 
+// }
+// SumOfDigits(32)
+
+
+
+//  create a function to return the total number of digit in a  number
+
+function NumberCount(number){
+    count = 0
+    while(number>0){
+        digit = number%10
+        count+=1
+        number=Math.floor(number / 10);
+
+    }
+    console.log(count);
+    
 }
-SumOfDigits(32)
+NumberCount(467892)
