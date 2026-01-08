d9087fc8a5c150cb7ef06d58a59c63b4fff26a5c
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-08 09:37:08 +0530

    // find squre root of each element in a an array

--- ArrayMethods/map.js
+++ ArrayMethods/map.js
@@ -7,4 +7,24 @@
 // }
 
 
-numbers = 
+// numbers = [2,3,4,5]
+
+// let result = numbers.map(num =>num**2)
+
+// console.log(result);
+
+// find squre root of each element in a an array
+// numbers = [4,9,19,35]
+
+// let result = numbers.map(num=>num**0.5)
+
+// console.log(result);
+
+// find the cube of each element in the array
+
+num = [1,2,3,4]
+
+let result = 
+
+
+
