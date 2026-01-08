e59b3abbba598a13101623677fb7f0646ce7895c
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-08 10:27:56 +0530

    asyn

--- Aysnchoronus/asyn.js
+++ Aysnchoronus/asyn.js
@@ -1,2 +1,16 @@
 // synchronosation and asynchronousation
 
+console.log('hai');
+setTimeout(()=>
+console.log("js"),2000
+)
+// avoid buffering
+
+// synch
+
+// each line waits for the pervious one to finish
+
+// asyn
+
+// here setTimeout sends to background
+// js continue execution
